a66292a536bab0376ca0ed583b8428422693b486
PVAK244 <khoapvade150321@fpt.edu.vn>
2022-03-29 03:37:15 +0700

    Add project files.

---
+++ AutomobileLibrary/DataAccess/CarDBContext.cs
@@ -0,0 +1,154 @@
+﻿using AutomobileLibrary.BussinessObject;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileLibrary.DataAccess
+{
+    public class CarDBContext : BaseDAL
+    {
+        private static CarDBContext instance = null;
+        private static readonly object instancLock = new object();
+        private CarDBContext() { }
+        public static CarDBContext Instance
+        {
+            get
+            {
+                lock (instancLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new CarDBContext();
+                    }
+                    return instance;
+                }
+            }
+        }
+        public IEnumerable<Car> GetCarList()
+        {
+            IDataReader dataReader = null;
+            string SQLSelect = "Select * from Cars";
+            var cars = new List<Car>();
+            try
+            {
+                dataReader = dataProvider.GetDataReader(SQLSelect, CommandType.Text, out connection);
+                while (dataReader.Read())
+                {
+                    cars.Add(new Car
+                    {
+                        CarID = dataReader.GetInt32(0),
+                        CarName = dataReader.GetString(1),
+                        Manufacturer = dataReader.GetString(2),
+                        Price = dataReader.GetDecimal(3),
+                        ReleaseYear = dataReader.GetInt32(4)
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                dataReader.Close();
+                CloseConnection();
+            }
+            return cars;
+        }
+        public Car GetCarByID(int carID)
+        {
+            Car car = GetCarList().SingleOrDefault(pro => pro.CarID == carID);
+            return car;
+        }
+        public void AddNew(Car car)
+        {
+            try
+            {
+                Car pro = GetCarByID(car.CarID);
+                if (pro == null)
+                {
+                    string SQLInsert = "Insert Cars values(@CarID,@CarName,@Manufacturer,@Price,@ReleasedYear)";
+                    var parameters = new List<SqlParameter>();
+                    parameters.Add(dataProvider.CreateParameter("@CarID", 4, car.CarID, DbType.Int32));
+                    parameters.Add(dataProvider.CreateParameter("@CarName", 50, car.CarName, DbType.String));
+                    parameters.Add(dataProvider.CreateParameter("@Manufacturer", 50, car.Manufacturer, DbType.String));
+                    parameters.Add(dataProvider.CreateParameter("@Price", 50, car.Price, DbType.Decimal));
+                    parameters.Add(dataProvider.CreateParameter("@ReleasedYear", 4, car.ReleaseYear, DbType.Int32));
+                    dataProvider.Insert(SQLInsert, CommandType.Text, parameters.ToArray());
+                }
+                else
+                {
+                    throw new Exception("The car is already exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+        public void Update(Car car)
+        {
+            try
+            {
+                Car pro = GetCarByID(car.CarID);
+                if (pro != null)
+                {
+                    string SQLUpdate = "Update Cars set [CarName] = @CarName, [Manufacturer] = @Manufacturer, [Price] = @Price, [ReleasedYear] = @ReleasedYear" +
+                        " where [CarID] = @CarID";
+                    var parameters = new List<SqlParameter>();
+                    parameters.Add(dataProvider.CreateParameter("@CarID", 4, car.CarID, DbType.Int32));
+                    parameters.Add(dataProvider.CreateParameter("@CarName", 50, car.CarName, DbType.String));
+                    parameters.Add(dataProvider.CreateParameter("@Manufacturer", 50, car.Manufacturer, DbType.String));
+                    parameters.Add(dataProvider.CreateParameter("@Price", 50, car.Price, DbType.Decimal));
+                    parameters.Add(dataProvider.CreateParameter("@ReleasedYear", 4, car.ReleaseYear, DbType.Int32));
+                    dataProvider.Update(SQLUpdate, CommandType.Text, parameters.ToArray());
+                }
+                else
+                {
+                    throw new Exception("The car is not already exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+        public void Remove(int carID)
+        {
+            try
+            {
+                Car pro = GetCarByID(carID);
+                if (pro != null)
+                {
+                    string SQLDelete = "Delete Cars where [CarID] = @CarID";
+                    var param = dataProvider.CreateParameter("@CarID", 4, carID, DbType.Int32);
+                    dataProvider.Delete(SQLDelete, CommandType.Text, param);
+                }
+                else
+                {
+                    throw new Exception("The car is does not already exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+    }
+}
